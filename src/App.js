45596d19c7687fe8c841c9dfb88447a175a34bad
Your Name <you@example.com>
2020-09-01 13:08:20 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth);
+
+  // const [resorceType, setResorceType] = useState("posts");
+  // const [items, setItems] = useState([]);
+
+  // useEffect(() => {
+  //   fetch(`https://jsonplaceholder.typicode.com/${resorceType}`)
+  //     .then((response) => response.json())
+  //     .then((json) => setItems(json));
+  // }, [resorceType]);
+
+  const handleResize = () => {
+    setwindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+  return () => {
+    window.removeEventListener("resize", handleResize);
+  }
+
+});
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      {/* <div className="btn">
+        <button onClick={() => setResorceType("posts")}>Posts</button>
+        <button onClick={() => setResorceType("users")}>Users</button>
+        <button onClick={() => setResorceType("comments")}>Comments</button>
+      </div>
+      <h1>{resorceType}</h1>
+      {items.map((item) => {
+        return <pre>{JSON.stringify(item)}</pre>;
+      })} */}
+
+      <div>{windowWidth}</div>
+    </>
   );
 }
 
